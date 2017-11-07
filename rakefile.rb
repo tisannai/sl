6c21b1c35adeae6d1515e08a511e9d6d62e7f949
Tero Isannainen <tero.isannainen@gmail.com>
2017-11-07 06:07:09 +0200

    Release build to ceedling.

--- rakefile.rb
+++ rakefile.rb
@@ -3,4 +3,11 @@ load "#{PROJECT_CEEDLING_ROOT}/lib/ceedling.rb"
 
 Ceedling.load_project
 
-task :default => %w[ test:all release ]
+task :default => %w[ test:all release publish ]
+
+require 'fileutils'
+task :publish do
+    Rake::Task["release"].invoke
+    FileUtils.cp( "build/release/libsl.so.0.0.1", "#{ENV['HOME']}/usr/lib" )
+    FileUtils.cp( "src/sl.h", "#{ENV['HOME']}/usr/include" )
+end
